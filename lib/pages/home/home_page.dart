cc4a5067862982c718ac4114126c7021c6ead42c
Wagner Araujo <araujowp@gmail.com>
2025-06-19 12:38:55 -0300

    ajustement bottom navigation bar

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -52,6 +52,7 @@ class _HomePageState extends State<HomePage> {
       ),
       body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         currentIndex: _currentIndex,
         onTap: (index) {
           setState(() {
@@ -72,8 +73,8 @@ class _HomePageState extends State<HomePage> {
             label: 'Login',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Login2',
+            icon: Icon(Icons.groups),
+            label: 'Logins',
           ),
         ],
       ),
